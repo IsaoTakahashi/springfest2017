e33e29ed64f064eb0fa3d6f1660ae462ddcbec1a
IsaoTakahashi <ricto415@gmail.com>
2017-11-24 14:26:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# springfest2017
+Spring Data REST Demo Project for Spring Fest 2017
+
+# Reference
+[Spring Fest 2017](http://springfest2017.springframework.jp/#session)
---
+++ src/main/java/itaka/demo/springfest/SpringfestApplication.java
@@ -0,0 +1,12 @@
+package itaka.demo.springfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfestApplication.class, args);
+	}
+}
